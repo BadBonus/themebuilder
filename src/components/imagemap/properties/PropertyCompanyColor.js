d1cf5e0473b02e80c14ac38dd29ff671af52b559
BadBonus <arturyes1993@gmail.com>
2020-10-23 11:20:05 +0300

    fix 123

--- src/components/imagemap/properties/PropertyCompanyColor.js
+++ src/components/imagemap/properties/PropertyCompanyColor.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Form, Input, Slider, Switch, Col, InputNumber, Row } from 'antd';
 import i18n from 'i18next';
-// mocks
-const mock_colors = ['red', 'green', 'blue'];
-
+// color1_hex
+import Cookies from 'universal-cookie';
 class PropertyCompanyColor extends Component {
 	renderColorItems = () => null;
 
@@ -11,17 +10,13 @@ class PropertyCompanyColor extends Component {
 		defaultColors: [],
 		changed: 0,
 		choosedColor: null,
+		colors: [],
 	};
 
 	changeColorOfElement = color => {
 		const { canvasRef, selectedItem } = this.props;
 		const { changed } = this.state;
 		const colors = [];
-
-		// console.log('item(1) fill');
-		// console.log(canvas.canvas.item(1)._objects[0].fill);
-		// canvas.canvas.item(1)._objects[0].fill='gold';
-		// console.log(canvas.canvas.item(1)._objects[0].fill);
 
 		if (color === 'default') {
 			selectedItem._objects.forEach((el, index) => {
@@ -47,11 +42,22 @@ class PropertyCompanyColor extends Component {
 	};
 
 	renderColorButtons = arrayOfColors =>
-		arrayOfColors.map((color, index) => (
-			<li key={index + Math.random()} className={color === this.state.choosedColor ? 'active' : ''}>
-				<button onClick={() => this.changeColorOfElement(color)} style={{ background: color }} />
-			</li>
-		));
+		arrayOfColors
+			.filter(color => color !== null && color !== undefined)
+			.map((color, index) => (
+				<li key={index + Math.random()} className={color === this.state.choosedColor ? 'active' : ''}>
+					<button onClick={() => this.changeColorOfElement(color)} style={{ background: color }} />
+				</li>
+			));
+
+	componentDidMount() {
+		const cookies = new Cookies();
+		const color1 = cookies.get('color1_hex');
+		const color2 = cookies.get('color2_hex');
+		const color3 = cookies.get('color3_hex');
+
+		this.setState({ colors: [color1, color2, color3] });
+	}
 
 	componentDidUpdate(oldProps) {
 		if (oldProps.selectedItem !== this.props.selectedItem) {
@@ -62,17 +68,19 @@ class PropertyCompanyColor extends Component {
 	render() {
 		return (
 			<div className="PropertyCompanyColor">
-				<h5>Choose main color</h5>
-				<ul>
-					<li className={'default' === this.state.choosedColor ? 'active' : ''}>
-						<button
-							disabled={this.state.changed === 0}
-							onClick={() => this.changeColorOfElement('default')}
-						/>
-					</li>
+				{this.renderColorButtons(this.state.colors).length ? <h5>Choose main color</h5> : null}
+				{this.renderColorButtons(this.state.colors).length ? (
+					<ul>
+						<li className={'default' === this.state.choosedColor ? 'active' : ''}>
+							<button
+								disabled={this.state.changed === 0}
+								onClick={() => this.changeColorOfElement('default')}
+							/>
+						</li>
 
-					{this.renderColorButtons(mock_colors)}
-				</ul>
+						{this.renderColorButtons(this.state.colors)}
+					</ul>
+				) : null}
 			</div>
 		);
 	}
